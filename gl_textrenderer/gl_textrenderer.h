4ff945619103e9015dff32bcc3b2026f08d9d9c7
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-12 11:10:33 -0400

    move Character struct to be private, add new members and ctor params

--- gl_textrenderer/gl_textrenderer.h
+++ gl_textrenderer/gl_textrenderer.h
@@ -14,29 +14,31 @@
 
 using namespace gl;
 
-struct Character {
-    unsigned int TextureID;  // opengl texture ID of the glyph
-    glm::ivec2   Size;       // Size of glyph (width and height of bitmap)
-    // bearing.x horizontal position relative to the origin
-    // bearing.y vertical position relative to the baseline
-    glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
-    // horizontal distance in 1/64th pixels from the origin to the next origin
-    unsigned int Advance;    // Offset to advance to next glyph
-};
+
 
 class gl_textrenderer
 {
 public:
-    gl_textrenderer(unsigned int screen_width, unsigned int screen_height);
+    gl_textrenderer(unsigned int screen_width, unsigned int screen_height, std::string& font_path);
     ~gl_textrenderer();
 
     void render_text();
 private:
+    struct m_character {
+        unsigned int TextureID;  // opengl texture ID of the glyph
+        glm::ivec2   Size;       // Size of glyph (width and height of bitmap)
+        // bearing.x horizontal position relative to the origin
+        // bearing.y vertical position relative to the baseline
+        glm::ivec2   Bearing;    // Offset from baseline to left/top of glyph
+        // horizontal distance in 1/64th pixels from the origin to the next origin
+        unsigned int Advance;    // Offset to advance to next glyph
+    };
     void load_ascii_characters();
     unsigned int create_shaders(std::string& vertex_src, std::string& fragment_src);
     unsigned int m_screen_width;
     unsigned int m_screen_height;
-    std::map<char, Character> m_characters;
+    std::string m_font_path;
+    std::map<char, m_character> m_characters;
     glm::mat4 m_projection;
     float m_x;
     float m_y;
