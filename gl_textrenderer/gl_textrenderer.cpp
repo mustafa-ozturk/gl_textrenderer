b47fda161f312b0876f2947b7f14244c64767938
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-12 11:10:45 -0400

    implement load_ascii_characters

--- gl_textrenderer/gl_textrenderer.cpp
+++ gl_textrenderer/gl_textrenderer.cpp
@@ -1,7 +1,9 @@
 #include "gl_textrenderer.h"
 
-gl_textrenderer::gl_textrenderer(unsigned int screen_width, unsigned int screen_height)
-    : m_screen_width(screen_width), m_screen_height(screen_height)
+gl_textrenderer::gl_textrenderer(unsigned int screen_width, unsigned int screen_height, std::string& font_path)
+    : m_screen_width(screen_width),
+      m_screen_height(screen_height),
+      m_font_path(font_path)
 {
     std::string vertex_shader = R"(
         #version 330 core
@@ -33,4 +35,71 @@ gl_textrenderer::gl_textrenderer(unsigned int screen_width, unsigned int screen_
     )";
 
     m_shader_program = create_shaders(vertex_shader, fragment_shader);
+}
+
+void gl_textrenderer::load_ascii_characters()
+{
+    FT_Library ft;
+    if (FT_Init_FreeType(&ft))
+    {
+        std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+        return;
+    }
+
+    // face == font
+    FT_Face face;
+    if (FT_New_Face(ft, m_font_path.c_str(), 0, &face))
+    {
+        std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+        return;
+    }
+
+    FT_Set_Pixel_Sizes(face, 0, 50);
+
+    // disable byte-alignment restriction
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    // load first 128 characters of ASCII set
+    for (unsigned char c = 0; c < 128; c++)
+    {
+        // Load character glyph
+        if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+        {
+            std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+            continue;
+        }
+        // generate texture
+        unsigned int texture;
+        glGenTextures(1, &texture);
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glTexImage2D(
+                GL_TEXTURE_2D,
+                0,
+                GL_RED,
+                face->glyph->bitmap.width,
+                face->glyph->bitmap.rows,
+                0,
+                GL_RED,
+                GL_UNSIGNED_BYTE,
+                face->glyph->bitmap.buffer
+        );
+        // set texture options
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        // now store character for later use
+        m_character character = {
+                texture,
+                glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+                glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+                static_cast<unsigned int>(face->glyph->advance.x)
+        };
+        m_characters.insert(std::pair<char, m_character>(c, character));
+    }
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    // destroy FreeType once we're finished
+    FT_Done_Face(face);
+    FT_Done_FreeType(ft);
 }
