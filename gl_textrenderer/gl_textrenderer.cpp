dd96483c21b4198da95f18c4f663e2ad769cf6a9
Mustafa Ozturk <mozturkpro@gmail.com>
2023-05-12 12:26:47 -0400

    better ordering in gl_textrenderer.cpp

--- gl_textrenderer/gl_textrenderer.cpp
+++ gl_textrenderer/gl_textrenderer.cpp
@@ -39,6 +39,97 @@ gl_textrenderer::gl_textrenderer(unsigned int screen_width, unsigned int screen_
     load_ascii_characters();
 }
 
+gl_textrenderer::~gl_textrenderer()
+{
+
+}
+
+
+void gl_textrenderer::render_text(std::string text, float x, float y, float scale)
+{
+//    centering code not sure how it will be part of API yet
+//    int textWidth = 0;
+//    int textHeight = 0;
+//    int count = 0;
+//    for (char c : text)
+//    {
+//        Character ch = Characters[c];
+//        // pick the biggest height in the text
+//        if (ch.Size.y > textHeight)
+//        {
+//            textHeight = ch.Size.y;
+//        }
+//        textWidth += ch.Advance >> 6;
+//        count++;
+//    }
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    int charCount = 0;
+    for (char c : text)
+    {
+        unsigned int VBO_chars, VAO_chars;
+
+        glGenVertexArrays(1, &VAO_chars);
+        glBindVertexArray(VAO_chars);
+        glGenBuffers(1, &VBO_chars);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_chars);
+        // reserve memory, later we will update the VBO's memory when rendering characters
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, nullptr, GL_DYNAMIC_DRAW);
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+
+        m_character ch = m_characters[c];
+
+        glUseProgram(m_shader_program);
+        glUniformMatrix4fv(glGetUniformLocation(m_shader_program, "projection"), 1, GL_FALSE, glm::value_ptr(m_projection));
+        glUniform3f(glGetUniformLocation(m_shader_program, "textColor"), 200/255.0, 60/255.0, 30/255.0);
+        glBindVertexArray(VAO_chars);
+
+        // TODO: remove first bearingX
+        float xpos = x + ch.Bearing.x * scale;;
+        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+
+        float width = ch.Size.x * scale;
+        float height = ch.Size.y * scale;
+        /*
+         * C      D ypos + height,
+         *
+         * A      B
+         *   xpos + width
+         * FREETYPE GLYPHS ARE REVERSED: 0,0  = top left
+         * */
+        // update VBO for each character
+        float char_vertices[6][4] = { // x,y,tx,ty
+                // first triangle
+                {xpos,     ypos + height,     0.0f, 0.0f}, // C
+                {xpos, ypos,                  0.0f, 1.0f}, // A
+                {xpos + width, ypos,          1.0f, 1.0f}, // B
+
+                // second triangle
+                {xpos,     ypos + height,     0.0f, 0.0f}, // C
+                {xpos + width, ypos,          1.0f, 1.0f}, // B
+                {xpos + width, ypos + height, 1.0f, 0.0f}  // D
+        };
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+        glBindBuffer(GL_ARRAY_BUFFER, VBO_chars);
+        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(char_vertices), char_vertices);
+
+        // render quad
+        glDrawArrays(GL_TRIANGLES, 0, 6);
+        x += (ch.Advance >> 6);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
+        glBindTexture(GL_TEXTURE_2D, 0);
+        glUseProgram(0);
+
+        charCount++;
+    }
+}
+
 void gl_textrenderer::load_ascii_characters()
 {
     FT_Library ft;
@@ -152,93 +243,4 @@ unsigned int gl_textrenderer::create_shader_program(std::string& vertex_src, std
     return shaderProgram;
 }
 
-void gl_textrenderer::render_text(std::string text, float x, float y, float scale)
-{
-//    centering code not sure how it will be part of API yet
-//    int textWidth = 0;
-//    int textHeight = 0;
-//    int count = 0;
-//    for (char c : text)
-//    {
-//        Character ch = Characters[c];
-//        // pick the biggest height in the text
-//        if (ch.Size.y > textHeight)
-//        {
-//            textHeight = ch.Size.y;
-//        }
-//        textWidth += ch.Advance >> 6;
-//        count++;
-//    }
-    glEnable(GL_BLEND);
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
-    int charCount = 0;
-    for (char c : text)
-    {
-        unsigned int VBO_chars, VAO_chars;
-
-        glGenVertexArrays(1, &VAO_chars);
-        glBindVertexArray(VAO_chars);
-        glGenBuffers(1, &VBO_chars);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO_chars);
-        // reserve memory, later we will update the VBO's memory when rendering characters
-        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, nullptr, GL_DYNAMIC_DRAW);
-        glEnableVertexAttribArray(0);
-        glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
-
-        m_character ch = m_characters[c];
-
-        glUseProgram(m_shader_program);
-        glUniformMatrix4fv(glGetUniformLocation(m_shader_program, "projection"), 1, GL_FALSE, glm::value_ptr(m_projection));
-        glUniform3f(glGetUniformLocation(m_shader_program, "textColor"), 200/255.0, 60/255.0, 30/255.0);
-        glBindVertexArray(VAO_chars);
-
-        // TODO: remove first bearingX
-        float xpos = x + ch.Bearing.x * scale;;
-        float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
-
-
-        float width = ch.Size.x * scale;
-        float height = ch.Size.y * scale;
-        /*
-         * C      D ypos + height,
-         *
-         * A      B
-         *   xpos + width
-         * FREETYPE GLYPHS ARE REVERSED: 0,0  = top left
-         * */
-        // update VBO for each character
-        float char_vertices[6][4] = { // x,y,tx,ty
-                // first triangle
-                {xpos,     ypos + height,     0.0f, 0.0f}, // C
-                {xpos, ypos,                  0.0f, 1.0f}, // A
-                {xpos + width, ypos,          1.0f, 1.0f}, // B
-
-                // second triangle
-                {xpos,     ypos + height,     0.0f, 0.0f}, // C
-                {xpos + width, ypos,          1.0f, 1.0f}, // B
-                {xpos + width, ypos + height, 1.0f, 0.0f}  // D
-        };
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, ch.TextureID);
-        glBindBuffer(GL_ARRAY_BUFFER, VBO_chars);
-        glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(char_vertices), char_vertices);
-
-        // render quad
-        glDrawArrays(GL_TRIANGLES, 0, 6);
-        x += (ch.Advance >> 6);
-
-        glBindBuffer(GL_ARRAY_BUFFER, 0);
-        glBindVertexArray(0);
-        glBindTexture(GL_TEXTURE_2D, 0);
-        glUseProgram(0);
-
-        charCount++;
-    }
-}
-
-gl_textrenderer::~gl_textrenderer()
-{
-
-}
 
